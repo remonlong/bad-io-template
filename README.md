6e4c262d9364b634b424bd22a419bd101d122fe8
remonlong <remonzilong@sian.com>
2018-06-05 10:18:29 +0800

    modified readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bad-io-template
 java 代码生成器
+
+ Map<String, String> map = new HashMap<String, String>();
+ map.put("fiedList", "中国人");
+ map.put("packageName", "com.bad.remon.io");
+ TemplateBuilder builder = new TemplateBuilder(".java");
+ // builder.genTemplate("C:\\Users\\admin\\Desktop\\模板文档.txt", "C:\\Users\\admin\\Desktop\\testCode.txt", map);
+ builder.genPojoBeanTemplate("C:\\Users\\admin\\Desktop\\testCode.txt", map);
+
+即可生生成一个简单的java bean 
--- src/test/java/TemplateLoadTest.java
+++ src/test/java/TemplateLoadTest.java
@@ -28,7 +28,7 @@ public class TemplateLoadTest {
         Map<String, String> map = new HashMap<String, String>();
         map.put("fiedList", "中国人");
         map.put("packageName", "com.bad.remon.io");
-        TemplateBuilder builder = new TemplateBuilder();
+        TemplateBuilder builder = new TemplateBuilder(".java");
         // builder.genTemplate("C:\\Users\\admin\\Desktop\\模板文档.txt", "C:\\Users\\admin\\Desktop\\testCode.txt", map);
         builder.genPojoBeanTemplate("C:\\Users\\admin\\Desktop\\testCode.txt", map);
     }
